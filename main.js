448a0ea065f2c23d1dee594de310404077e38261
Vitoria-Daris-Bender <108440774+Vitoria-Daris-Bender@users.noreply.github.com>
2023-12-01 08:54:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const shaun = document.querySelector(".shaun");
+const cogu = document.querySelector(".cogu");
+const score = document.querySelector(".score");
+let aJump = false;
+let count = 0;
+
+
+
+document.addEventListener("keydown", (e)=> {
+    if ((e.code === "ArrowUp") | (e.code === "Space")) {
+        jump();
+    }
+ }); 
+
+function jump() {
+    if(!shaun.classList.contains("jump")) {
+        shaun.classList.add("jump");
+        aJump = true;
+
+        setTimeout(() => {
+            shaun.classList.remove("jump");
+            aJump = false;
+        }, 1000);
+    }
+}
+
+setInterval(() => {
+    let shaunBottom = parseInt(
+        window.getComputedStyle(shaun).getPropertyValue("bottom")
+    );
+    let coguLeft = parseInt(
+        window.getComputedStyle(cogu).getPropertyValue("left")
+    );
+
+    if(coguLeft > 40 && coguLeft < 260 && shaunBottom <= 50 && !aJump) {
+        alert(`Game Over!\n Você foi intoxicado por um Amanita Muscaria.\n Seu score foi: ${count}`);
+        count = 0;
+    }
+
+    count++;
+    score.innerHTML = `SCORE: ${count}`;
+}, 100);
